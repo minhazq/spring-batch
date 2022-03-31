d030db13622bfce89be5a186388f4476fe1624f2
minhazq <minhazq@gmail.com>
2022-03-31 16:13:00 -0400

    first commit

---
+++ src/test/java/com/minhaj/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaj.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
